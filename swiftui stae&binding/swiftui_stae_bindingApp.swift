9455b4fc203dd6ef54520b75a3a0938c2453a375
andrew <andyhaz1970@gmail.com>
2024-03-05 21:50:43 -0600

    Initial Commit

---
+++ swiftui stae&binding/swiftui_stae_bindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_stae_bindingApp.swift
+//  swiftui stae&binding
+//
+//  Created by andyhaz on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_stae_bindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
