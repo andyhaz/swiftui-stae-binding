87fc9a6e725c52eebd630c43b1ae1e4a3e036c96
andrew <andyhaz1970@gmail.com>
2024-03-05 22:05:23 -0600

    v0.1

--- swiftui stae&binding/ContentView.swift
+++ swiftui stae&binding/ContentView.swift
@@ -7,18 +7,49 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct customButton1: View {
+    
+    @Binding var placeHolder: String
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("b1"){self.placeHolder = "update" }
         }
-        .padding()
+    }
+}
+
+struct customButton2: View {
+    
+    @Binding var placeHolder: String
+    
+    var body: some View {
+        VStack {
+           // Button("b1"){self.placeHolder = "update" }
+            Text("b2")
+                .frame(width: 50,height: 50)
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+                .background(.blue)
+                .onTapGesture {
+                    self.placeHolder = "update2"
+                }
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @State var plaeHolder = "plae holder"
+    
+    var body: some View {
+        VStack {
+            Text("\(self.plaeHolder)")
+            customButton1(placeHolder: self.$plaeHolder)
+            customButton2(placeHolder: self.$plaeHolder)
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+//
